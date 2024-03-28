3f7f2cfcc21e1091513c1131f4614ab21a9a57ce
Marc Bernstein <marcbernstein98@gmail.com>
2024-03-27 21:20:11 -0400

    add debug logs to win screen buttons

--- Assets/Scripts/WinScreen/UI_Manager.cs
+++ Assets/Scripts/WinScreen/UI_Manager.cs
@@ -5,10 +5,13 @@ public class UI_Manager : MonoBehaviour
 {
     public void RestartGame()
     {
+        Debug.Log("Restart game pressed");
         SceneManager.LoadScene("Pong");
     }
 
-    public void QuitGame(){
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame pressed");
         Application.Quit();
     }
 }
